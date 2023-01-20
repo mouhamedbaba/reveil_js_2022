b2a0485e846d53db272cb5b6e0ccf49bce11ddda
BABALAMOTTE <lamottelymouhamed@gmail.com>
2023-01-20 12:58:11 +0000

    exo 3

--- js/index.js
+++ js/index.js
@@ -6,13 +6,15 @@ button.addEventListener("click", function () {
     const value1 = Number(input1.value)
     const value2 = Number(input2.value)
 
-    console.log(input1)
+    console.log("vous avez saisi " + value1 + " et " + value2)
 
-    console.log('le produit ' + value1 + '*' + value2 + 'est  :')
+    // console.log('le produit ' + value1 + '*' + value2 + 'est  :')
 
-    if (value1 * value2 <= 0) {
-        console.log('negatif ')
-    } else {
-        console.log('positif')
+    if (value1 > value2) {
+        console.log( value1 + " > " + value2)
+    } else if (value1 === value2) {
+        console.log( value2 + " = " + value1)
+    }else {
+        console.log( value2 + " < " + value1)
     }
-}); 
+});
