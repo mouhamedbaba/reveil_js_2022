acb7bc54e426cb2f38dbf57a3dff6f2f1d36edda
BABALAMOTTE <lamottelymouhamed@gmail.com>
2023-01-20 15:42:19 +0000

    exo 5

--- js/index.js
+++ js/index.js
@@ -4,17 +4,18 @@ var button = document.getElementById("send-btn")
 
 button.addEventListener("click", function () {
     const value1 = Number(input1.value)
-    const value2 = Number(input2.value)
+    // const value2 = Number(input2.value)
 
-    console.log("vous avez saisi " + value1 + " et " + value2)
+    
+
+    console.log("vous avez saisi " + value1 )
 
     // console.log('le produit ' + value1 + '*' + value2 + 'est  :')
+ 
+        if (value1 >= 0 && value1 <=10  ){
+            console.log('ce nombre est compris entre 0 et 10')
+        } else{
+            console.log("ce nombre n'est pas compris entre 0 et 10")
+        }
 
-    if (value1 > value2) {
-        console.log( value1 + " > " + value2)
-    } else if (value1 === value2) {
-        console.log( value2 + " = " + value1)
-    }else {
-        console.log( value2 + " < " + value1)
-    }
 });
