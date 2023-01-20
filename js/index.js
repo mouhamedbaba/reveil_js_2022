046e5d6e885104255d00fbcb838509b86916bb22
BABALAMOTTE <lamottelymouhamed@gmail.com>
2023-01-20 16:24:13 +0000

    exo 6

--- js/index.js
+++ js/index.js
@@ -3,19 +3,11 @@ var input2 = document.getElementById("value2")
 var button = document.getElementById("send-btn")
 
 button.addEventListener("click", function () {
+    const tab = ["0", "janvier", "fevrier", "mars" , "avril", "mai", "juin", "juillet", "aout", "septembre" ,"octobre", "novembre" , "decembre"]
     const value1 = Number(input1.value)
-    // const value2 = Number(input2.value)
-
-    
-
-    console.log("vous avez saisi " + value1 )
-
-    // console.log('le produit ' + value1 + '*' + value2 + 'est  :')
- 
-        if (value1 >= 0 && value1 <=10  ){
-            console.log('ce nombre est compris entre 0 et 10')
-        } else{
-            console.log("ce nombre n'est pas compris entre 0 et 10")
-        }
-
-});
+          if(value1 > 0 && value1 <= 12) { 
+              console.log("vous etes né en  "+ tab[value1])
+         } else{
+            console.log(value1 + " ne corespond a aucun moi")
+         }
+}); 
