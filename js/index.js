40dd9170071a3c23b228f6480d32787b6be36cf0
BABALAMOTTE <lamottelymouhamed@gmail.com>
2023-01-24 11:54:33 +0000

    fontion pour dire l'age en fontion de la date de naissance

--- js/index.js
+++ js/index.js
@@ -1,36 +1,24 @@
 const inputfirstNumber = document.getElementById("firstNumber")
-const inputsecondNumber = document.getElementById("secondNumber")
-const inputoperateur = document.getElementById("operateur")
+// const inputsecondNumber = document.getElementById("secondNumber")
+// const inputoperateur = document.getElementById("operateur")
 const button = document.getElementById("send-btn")
 
 button.addEventListener("click", function (e) {
     const valuefirstNumber = Number(inputfirstNumber.value)
-    const valuesecondNumber = Number(inputsecondNumber.value)
-    const valueoperateur = inputoperateur.value
-
-     e.preventDefault()
-    // console.log(valuefirstNumber)
-    let result =  operations(valuefirstNumber, valueoperateur, valuesecondNumber)
-    console.log(result)
+    // const valuesecondNumber = Number(inputsecondNumber.value)
+    // const valueoperateur = inputoperateur.value
+    e.preventDefault()
+       let resturnAge =  Age (valuefirstNumber) 
+       console.log(resturnAge)
 });
-
-function operations(valuefirstNumber, operateur, valuesecondNumber) {
-    if (operateur === "+") {
-        const somme = valuefirstNumber + valuesecondNumber
-        return somme
-        console.log(somme)
+ function Age (valuefirstNumber){
+    const age = 2023 - valuefirstNumber
+    if (valuefirstNumber < 2023) {
+        return age
+        console.log("Vous avez " + age + " ans ")
+    } else if (valuefirstNumber === 2023) {
+         console.log("vous venez tous juste de naitre")
+    } else {
+        console.log("vous ne  pouvez pas etre nee apres 2023 quand meme !")
     }
-    if (operateur === "-") {
-        const soustraction = valuefirstNumber - valuesecondNumber
-        console.log(soustraction)
-    }
-    if (operateur === "*") {
-        const produit = valuefirstNumber * valuesecondNumber
-        console.log(produit)
-    }
-    if (operateur === "%") {
-        const modulo = valuefirstNumber % valuesecondNumber
-        console.log(modulo)
-    }
-
-}
+ }
