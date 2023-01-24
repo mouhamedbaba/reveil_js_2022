fa37868330d37978bda9268e172337ae1eb25b14
BABALAMOTTE <lamottelymouhamed@gmail.com>
2023-01-24 11:31:01 +0000

    end de la fonction des operation

--- js/index.js
+++ js/index.js
@@ -3,25 +3,34 @@ const inputsecondNumber = document.getElementById("secondNumber")
 const inputoperateur = document.getElementById("operateur")
 const button = document.getElementById("send-btn")
 
-button.addEventListener("click", function () {
+button.addEventListener("click", function (e) {
     const valuefirstNumber = Number(inputfirstNumber.value)
     const valuesecondNumber = Number(inputsecondNumber.value)
     const valueoperateur = inputoperateur.value
-          
+
+     e.preventDefault()
+    // console.log(valuefirstNumber)
+    let result =  operations(valuefirstNumber, valueoperateur, valuesecondNumber)
+    console.log(result)
 });
-function somme (){
-  const somme = valuefirstNumber + valuesecondNumber
-    console.log(somme)
-}
-function soustraction (){
-  const  soustraction = valuefirstNumber - valuesecondNumber
-    console.log(soustraction)
-}
-function produit (){
-  const produi = valuefirstNumber * valuesecondNumber
-    console.log(produi)
-}
-function modulo (){
-  const  modulo = valuefirstNumber % valuesecondNumber
-    console.log(modulo)
+
+function operations(valuefirstNumber, operateur, valuesecondNumber) {
+    if (operateur === "+") {
+        const somme = valuefirstNumber + valuesecondNumber
+        return somme
+        console.log(somme)
+    }
+    if (operateur === "-") {
+        const soustraction = valuefirstNumber - valuesecondNumber
+        console.log(soustraction)
+    }
+    if (operateur === "*") {
+        const produit = valuefirstNumber * valuesecondNumber
+        console.log(produit)
+    }
+    if (operateur === "%") {
+        const modulo = valuefirstNumber % valuesecondNumber
+        console.log(modulo)
+    }
+
 }
