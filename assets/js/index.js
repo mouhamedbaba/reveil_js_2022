146b7ccb140fe401fb08295eb65f9c8251c3a67a
BABALAMOTTE <lamottelymouhamed@gmail.com>
2023-02-01 11:25:35 +0000

    tableau html pas fini

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,10 @@
-const liste = document.getElementById("liste")
-const Alphabet = ['a', 'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' ]
-let valeur =  Alphabet
-for (var i = 0; i < 26; i++ ) {
-  console.log(i + Alphabet[i] )
-  const li = document.createElement("li")
-  li.innerHTML = Alphabet[i]
-  liste.appendChild(li)
- }
- 
+const input = document.getElementById("input")
+const button = document.getElementById("button")
+
+button.addEventListener("click", function (e){
+     const saisie = input.value
+     document.getElementById("t1").innerHTML = saisie
+  
+     e.preventDefault()
+});
+
